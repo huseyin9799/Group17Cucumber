93f6d76404cc43cd3170b95c4672fea27947ad13
Huseyin <huseyin.demir.harmony@gmail.com>
2020-11-06 15:32:45 -0600

    Working login feature

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,20 +1,24 @@
 package com.vytrack.step_definitions;
 
 import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
 
 public class LoginStepDefinitions {
 
     LoginPage loginPage = new LoginPage();
+    WebDriver driver = Driver.getDriver();
 
     @Given("{string} is on the login page")
     public void is_on_the_login_page(String string) {
         String url = ConfigurationReader.getProperty("url");
-        Driver.getDriver().get(url);
+        driver.get(url);
     }
 
     @When("{string} enters username and password and clicks log in")
@@ -32,10 +36,13 @@ public class LoginStepDefinitions {
         String password = ConfigurationReader.getProperty("vytrack.password");
 
         loginPage.login(username,password);
+        BrowserUtils.wait(3);
     }
 
     @Then("{string} should be on Dashboard")
     public void should_be_on_dashboard(String string) {
-
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Dashboard";
+        Assert.assertEquals("Titles aren't matching!",actualTitle,expectedTitle);
     }
 }
